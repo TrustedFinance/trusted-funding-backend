0c5c13e1b7ef46226c38745454eebf0ab547f490
jheart-vic <adebowalevictorjheart@gmail.com>
2025-10-24 10:14:39 +0100

    update admin

--- crypto-backend/src/middlewares/admin.js
+++ crypto-backend/src/middlewares/admin.js
@@ -1,16 +1,9 @@
-import User from "../models/User.js";
-
-
- const admin = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.user._id).select("role");
-    if (!user || user.role !== "admin") {
-      return res.status(403).json({ message: "Admin role required" });
-    }
-    next();
-  } catch (err) {
-    return res.status(500).json({ message: "Server error" });
+// middlewares/admin.js
+const admin = (req, res, next) => {
+  if (!req.user || req.user.role !== 'admin') {
+    return res.status(403).json({ success: false, message: 'Admin role required' });
   }
+  next();
 };
 
 export default admin;
