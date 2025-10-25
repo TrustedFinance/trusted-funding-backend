51f0d9e8f8dbb6270550492bc91327f9f78c3b53
jheart-vic <adebowalevictorjheart@gmail.com>
2025-10-25 09:49:39 +0100

    make plans public

--- crypto-backend/src/routes/investments.js
+++ crypto-backend/src/routes/investments.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import auth from '../middlewares/auth.js';
 import { createInvestment, getUserInvestments } from '../controllers/investmentController.js';
-import { getLeaderboard } from '../controllers/adminController.js';
+import { getAllPlans, getLeaderboard } from '../controllers/adminController.js';
 
 const router = Router();
 
 router.post('/', auth, createInvestment);
 router.get('/', auth, getUserInvestments);
+router.get('/plans', getAllPlans)
 router.get('/leaderboard', auth, getLeaderboard);
 export default router;
